aab97064223914d84c4d11514701bc3a2b77bb34
ALEX-PC\Alex <alex@altrec.ru>
2024-04-23 10:58:03 +0500

    Revert "Merge pull request #3 from AlTrec1976/feature/tasck002"
    This reverts commit 2f4543b7a4614458f49cca457bb2e72ad4dab852, reversing
    changes made to 04ece0c03e7c8ab221ac1f656002101d5b570801.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,2 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, Git!");
-        int a = 23;
-        int b = 42;
-
-        Console.WriteLine(a + b);
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Git!");
